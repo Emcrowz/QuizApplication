f13e96c4269a09dee3973f202d9b9370396b5df5
Martynas Vrubliauskas <martynas.vru@gmail.com>
2025-01-10 19:13:03 +0200

    Initial project setup

---
+++ quizapplication.client/src/App.tsx
@@ -0,0 +1,53 @@
+﻿import { useEffect, useState } from 'react';
+import './App.css';
+
+interface Question {
+    id: number;
+    title: string;
+    choises: string[];
+    solution: string;
+    points: number;
+}
+
+function App() {
+    const [questions, setQuestions] = useState<Question[]>();
+
+    useEffect(() => {
+        populateQuestionData();
+    }, []);
+
+    const contents = questions === undefined
+        ? <p><em>Loading... Please refresh once the ASP.NET backend has started. See <a href="https://aka.ms/jspsintegrationreact">https://aka.ms/jspsintegrationreact</a> for more details.</em></p>
+        : <table className="table-striped table" aria-labelledby="tableLabel">
+            <thead>
+                <tr>
+                    <th>Title</th>
+                </tr>
+            </thead>
+            <tbody>
+                {questions.map(question =>
+                    <tr key={question.id}>
+                        <td>{question.title}</td>
+                    </tr>
+                )}
+            </tbody>
+        </table>;
+
+    return (
+        <div>
+            <h1 id="tableLabel">Quiz</h1>
+            <p>This component demonstrates fetching data from the server.</p>
+            {contents}
+        </div>
+    );
+
+    async function populateQuestionData() {
+        const response = await fetch('questions');
+        if (response.ok) {
+            const data = await response.json();
+            setQuestions(data);
+        }
+    }
+}
+
+export default App;
